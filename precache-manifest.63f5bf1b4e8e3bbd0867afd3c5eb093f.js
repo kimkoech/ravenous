dfb81bf4195357baa658928a1dcfc1ce9704b84e
Billy Koech <bkkoech@college.harvard.edu>
2020-06-05 21:43:35 -0400

    Updates

---
+++ precache-manifest.63f5bf1b4e8e3bbd0867afd3c5eb093f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "199ad1e8e5ca76029b68a8b6566840f0",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "6c44bdfc36f74d2b16f3",
+    "url": "/ravenous/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "50e845aa44183e114c04",
+    "url": "/ravenous/static/js/2.7e2ea1da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ravenous/static/js/2.7e2ea1da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c44bdfc36f74d2b16f3",
+    "url": "/ravenous/static/js/main.284619ca.chunk.js"
+  },
+  {
+    "revision": "39e841b39d02e0e6343f",
+    "url": "/ravenous/static/js/runtime-main.3824ce30.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ravenous/static/media/logo.5d5d9eef.svg"
+  }
+]);
